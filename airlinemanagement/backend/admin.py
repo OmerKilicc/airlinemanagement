52d37172a0890c99d94c96ca4a920b26fc2352c2
OmerKilic <omeerfkiliic@gmail.com>
2025-01-28 00:16:41 +0300

    Update admin.py

--- airlinemanagement/backend/admin.py
+++ airlinemanagement/backend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Airplane,Flight,Reservation
 
 # Register your models here.
+modelsToRegister = [Airplane,Flight,Reservation]
+admin.site.register(modelsToRegister)
